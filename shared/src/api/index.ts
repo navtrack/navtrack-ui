a7adc03cab6b23d5a2c1a318a1368b5645da37a3
Andrei Sabau <andrei@codeagency.com>
2022-10-14 06:18:59 +0300

    5

--- shared/src/api/index.ts
+++
@@ -1,5 +0,0 @@
-export * from "./model/custom/AssetRoleType";
-export * from "./model/custom/UnitsType";
-export * from "./authAxiosInstance";
-export * from "./axiosInstance";
-export * from "./index-generated";
