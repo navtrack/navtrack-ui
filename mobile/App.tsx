ff39c971fb0edf654cc8120c7916ffb50077ec9f
Andrei Sabau <andrei@codeagency.com>
2022-10-21 02:04:25 +0300

    Update App.tsx

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,34 +1,32 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import React from "react";
+import { IntlProvider } from "react-intl";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { QueryClient, QueryClientProvider } from "react-query";
 import { RecoilRoot, useRecoilState } from "recoil";
+import { AuthenticationProvider } from "./src/components/app/AuthenticationProvider";
+import { ConfigProvider } from "./src/components/app/ConfigProvider";
+import { SettingsProvider } from "./src/components/app/SettingsProvider";
+import { Navigation } from "./src/components/navigation/Navigation";
+import translations from "./src/translations";
+
+const queryClient = new QueryClient();
 
 export default function App() {
   return (
-    <RecoilRoot>
-      <InnerApp />
-    </RecoilRoot>
+    <SafeAreaProvider>
+      <RecoilRoot>
+        <QueryClientProvider client={queryClient}>
+          <ConfigProvider>
+            <SettingsProvider>
+              <AuthenticationProvider>
+                <IntlProvider locale="en" messages={translations["en"]}>
+                  <Navigation />
+                </IntlProvider>
+              </AuthenticationProvider>
+            </SettingsProvider>
+          </ConfigProvider>
+        </QueryClientProvider>
+      </RecoilRoot>
+    </SafeAreaProvider>
   );
 }
-
-function InnerApp() {
-  // const [x, setX] = useRecoilState(parsingAtom);
-  return (
-    <View style={styles.container}>
-      <TouchableOpacity>
-        <Text className="text-red-700">
-          Open up App.tsx to start working on your app!
-        </Text>
-        <StatusBar style="auto" />
-      </TouchableOpacity>
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center"
-  }
-});
